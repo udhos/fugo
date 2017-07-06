9e8c81033d66ccda8160d025b05492ad4d2eb31f
udhos <udhos>
2017-07-06 17:35:20 -0300

    Document shared delta-time based update helpers.

--- future/future.go
+++ future/future.go
@@ -4,6 +4,7 @@ import (
 	"time"
 )
 
+// Fuel calculates new value after elap delta time interval.
 func Fuel(initial float32, elap time.Duration) float32 {
 	rate := float32(1.0 / 3.0) // 1 unit every 3 seconds
 	fuel := initial + rate*float32(int64(elap))/1000000000
@@ -13,6 +14,7 @@ func Fuel(initial float32, elap time.Duration) float32 {
 	return fuel
 }
 
+// CannonX calculates new value after elap delta time interval.
 func CannonX(initial float32, rate float32, elap time.Duration) (float32, float32) {
 	x := initial + rate*float32(int64(elap))/1000000000
 	switch {
