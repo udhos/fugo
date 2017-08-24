093ff5f98a2a23047ead666a76cf41f69f492229
udhos <udhos>
2017-08-24 12:06:03 -0300

    Intersect.

--- arena/collision.go
+++ arena/collision.go
@@ -13,7 +13,15 @@ type box interface {
 }
 
 func intersect(b1, b2 box) bool {
-	return false
+	b1x1, b1y1, b1x2, b1y2 := b1.Bounding()
+	b2x1, b2y1, b2x2, b2y2 := b2.Bounding()
+
+	noOverlap := b1x1 > b2x2 ||
+		b2x1 > b1x2 ||
+		b1y1 > b2y2 ||
+		b2y1 > b1y2
+
+	return !noOverlap
 }
 
 func detectCollision(w *world, now time.Time) {
@@ -24,15 +32,16 @@ func detectCollision(w *world, now time.Time) {
 
 	for _, m := range w.missileList {
 		mY := float64(future.MissileY(m.CoordY, m.Speed, now.Sub(m.Start)))
-		up := m.Team == 0
-		mr := unit.MissileBox(-1, 1, float64(m.CoordX), mY, fieldTop, cannonBottom, up)
+		mUp := m.Team == 0
+		mr := unit.MissileBox(-1, 1, float64(m.CoordX), mY, fieldTop, cannonBottom, mUp)
 
 		for _, p := range w.playerTab {
 			if m.Team == p.team {
 				continue
 			}
 			cX, _ := future.CannonX(p.cannonCoordX, p.cannonSpeed, now.Sub(p.cannonStart))
-			cr := unit.CannonBox(-1, 1, float64(cX), fieldTop, cannonBottom, up)
+			cUp := p.team == 0
+			cr := unit.CannonBox(-1, 1, float64(cX), fieldTop, cannonBottom, cUp)
 			if intersect(mr, cr) {
 				log.Printf("collision: %v %v", m, p)
 			}
